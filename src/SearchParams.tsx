1845ab7c97c181aa59672411786a685339cb62cf
Johan <johanur.rahman@pm.me>
2023-09-25 11:30:24 +0600

    useEffect added with api fetching for Pets

--- src/SearchParams.tsx
+++ src/SearchParams.tsx
@@ -1,12 +1,27 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Pet from './Pet';
 
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
 
 const SearchParams = () => {
   const [location, setLocation] = useState('');
   const [animal, setAnimal] = useState('');
+  const [pets, setPets] = useState([]);
   const [breed, setBreed] = useState('');
   const breeds = [];
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
+
 
   return (
     <div className="search-params">
@@ -54,6 +69,12 @@ const SearchParams = () => {
         </label>
         <button type="submit">Submit</button>
       </form>
+
+      {
+        pets.map((pet) => (
+          <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+        ))
+      }
     </div>
   );
 };
