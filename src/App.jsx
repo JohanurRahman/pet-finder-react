15ed40adcc5bb3a48d246cd2eccfa474bb8f3766
Johan <johanur.rahman@pm.me>
2023-09-26 00:50:10 +0600

    React query configuration/initialization added

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,29 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import SearchParams from './SearchParams';
 import Details from './Details';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adobe Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />}></Route>
-        <Route path="/" element={<SearchParams />}></Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adobe Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />}></Route>
+          <Route path="/" element={<SearchParams />}></Route>
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
