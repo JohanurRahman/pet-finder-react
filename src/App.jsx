8405ba1ca7965d91fed13801312237d2a9b52068
Johan <johanur.rahman@pm.me>
2023-09-26 00:07:29 +0600

    Basic react router example added
    React capabilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import SearchParams from './SearchParams';
+import Details from './Details';
 
 const App = () => {
   return (
-    <div>
-      <h1>Adobe Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adobe Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />}></Route>
+        <Route path="/" element={<SearchParams />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
