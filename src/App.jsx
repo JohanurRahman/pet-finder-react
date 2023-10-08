1081fe27ded53b734ebc3cc0da3836db06540ef8
Johanur Rahman <johanur.rahman@pm.me>
2023-10-09 02:30:49 +0600

    Revert "Server side rendering added"
    This reverts commit 376d2e52a5d78859362f3cf4d51e4a2d70266307.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { lazy, Suspense, useState } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AdoptedPetContext from './AdoptedPetContext';
 
-const Details = lazy(() => import('./Details'));
-const SearchParams = lazy(() => import('./SearchParams'));
+const Details = lazy(() => import ('./Details'));
+const SearchParams = lazy(() => import ('./SearchParams'));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,18 +19,22 @@ const queryClient = new QueryClient({
 const App = () => {
   const adoptedPet = useState(null);
   return (
-    <QueryClientProvider client={queryClient}>
-      <AdoptedPetContext.Provider value={adoptedPet}>
-        <header>
-          <Link to="/">Adobe Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />}></Route>
-          <Route path="/" element={<SearchParams />}></Route>
-        </Routes>
-      </AdoptedPetContext.Provider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/">Adobe Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />}></Route>
+            <Route path="/" element={<SearchParams />}></Route>
+          </Routes>
+        </AdoptedPetContext.Provider>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 
-export default App;
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
